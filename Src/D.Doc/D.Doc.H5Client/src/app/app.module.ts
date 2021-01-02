715e8d42684322758f6eb38e1a9b4d0b0d1ef703
Alger <AlgerDu@outlook.com>
2021-01-02 17:02:35 +0800

    ng add ng-zorro-antd

--- Src/D.Doc/D.Doc.H5Client/src/app/app.module.ts
+++ Src/D.Doc/D.Doc.H5Client/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
