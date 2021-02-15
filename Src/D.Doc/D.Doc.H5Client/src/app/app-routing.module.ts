d47321017668df9d63b298cdbd68cfeb01585a6d
Alger <AlgerDu@outlook.com>
2021-02-15 15:57:07 +0800

    添加新的页面

--- Src/D.Doc/D.Doc.H5Client/src/app/app-routing.module.ts
+++ Src/D.Doc/D.Doc.H5Client/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DbDocComponent } from './pages/db-doc/db-doc.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/db' },
+  { path: 'db', component: DbDocComponent },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
---
+++ Src/D.Doc/D.Doc.H5Client/src/app/pages/db-doc/db-doc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'd-db-doc',
+  templateUrl: './db-doc.component.html',
+  styleUrls: ['./db-doc.component.less']
+})
+export class DbDocComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
