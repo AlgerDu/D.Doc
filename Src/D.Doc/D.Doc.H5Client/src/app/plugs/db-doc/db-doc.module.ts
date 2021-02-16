6e965d0a924618a56e3dd39c4821aa3bf18911d2
Alger <AlgerDu@outlook.com>
2021-02-16 23:06:56 +0800

    Update db-doc.module.ts

--- Src/D.Doc/D.Doc.H5Client/src/app/plugs/db-doc/db-doc.module.ts
+++ Src/D.Doc/D.Doc.H5Client/src/app/plugs/db-doc/db-doc.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 import { DbDocRoutingModule } from './db-doc-routing.module';
 import { DbDocComponent } from './db-doc.component';
@@ -11,11 +14,14 @@ import { DbTablesComponent } from './pages/db-tables.component';
 
 @NgModule({
   imports: [
+    CommonModule,
+    FormsModule,
     DbDocRoutingModule,
     NzButtonModule,
     NzGridModule,
     NzCardModule,
-    NzTableModule
+    NzTableModule,
+    NzCheckboxModule
   ],
   declarations: [
     DbDocComponent,
