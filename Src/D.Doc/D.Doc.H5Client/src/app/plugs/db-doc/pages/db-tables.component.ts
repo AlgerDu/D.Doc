3d3668b346c1646212267b5703b2850bbbbbd4e2
Alger <AlgerDu@outlook.com>
2021-02-16 23:07:05 +0800

    终于有点样子了

--- Src/D.Doc/D.Doc.H5Client/src/app/plugs/db-doc/pages/db-tables.component.ts
+++ Src/D.Doc/D.Doc.H5Client/src/app/plugs/db-doc/pages/db-tables.component.ts
@@ -7,9 +7,108 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DbTablesComponent implements OnInit {
 
-  constructor() { }
+  schemaDatas: DbSchemaData[];
+  schemaDateType = DbSchemaType;
+  headers = ["ID", "Name", "Age", "Gender", "Country"];
 
-  ngOnInit(): void {
+  constructor() {
+    this.schemaDatas = [
+      {
+        isSelect: false,
+        type: DbSchemaType.Table,
+        data: {
+          name: "doc",
+          text: "文档"
+        }
+      },
+      {
+        isSelect: true,
+        type: DbSchemaType.Field,
+        data: {
+          name: "id",
+          text: "",
+          enableNull: false,
+          description: "主键"
+        }
+      },
+      {
+        isSelect: false,
+        type: DbSchemaType.Field,
+        data: {
+          name: "name",
+          text: "文档名称",
+          enableNull: false,
+          description: ""
+        }
+      },
+      {
+        isSelect: false,
+        type: DbSchemaType.Field,
+        data: {
+          name: "version",
+          text: "版本",
+          enableNull: false,
+          description: ""
+        }
+      },
+      {
+        isSelect: false,
+        type: DbSchemaType.Table,
+        data: {
+          name: "doc_tree",
+          text: "文档"
+        }
+      },
+      {
+        isSelect: true,
+        type: DbSchemaType.Field,
+        data: {
+          name: "id",
+          text: "",
+          enableNull: false,
+          description: "主键"
+        }
+      },
+      {
+        isSelect: false,
+        type: DbSchemaType.Field,
+        data: {
+          name: "name",
+          text: "文档名称",
+          enableNull: false,
+          description: ""
+        }
+      },
+      {
+        isSelect: false,
+        type: DbSchemaType.Field,
+        data: {
+          name: "version",
+          text: "版本",
+          enableNull: false,
+          description: ""
+        }
+      }
+    ]
   }
 
+  ngOnInit(): void {
+
+  }
+
+}
+
+export class DbSchemaData {
+  public isSelect: boolean;
+  public type: DbSchemaType;
+  public data: { [key: string]: any };
+
+  constructor() {
+    this.isSelect = false
+  }
+}
+
+export enum DbSchemaType {
+  Table,
+  Field
 }
