55600bf00b407bcc226b2d3b4551012bee1bf224
Alger <AlgerDu@outlook.com>
2021-02-20 23:01:41 +0800

    Create IProject.cs

---
+++ Src/D.Doc/D.Doc.Core/Interfaces/IProject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Doc.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IProject
+    {
+        long ID { get; set; }
+
+        string Name { get; set; }
+
+        string Description { get; set; }
+    }
+}
