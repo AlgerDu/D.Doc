e1b66c9c53d2235ab3fa2fd9f31523795d924a49
Alger <AlgerDu@outlook.com>
2021-02-27 22:51:39 +0800

    Update DocContext.cs

--- Src/D.Doc/D.Doc.Domai.Repository.EF/DocContext.cs
+++ Src/D.Doc/D.Doc.Domai.Repository.EF/DocContext.cs
@@ -6,19 +6,33 @@ using System.Text;
 
 namespace D.Doc.Domain.Repository
 {
+    /// <summary>
+    /// ef context for doc
+    /// </summary>
     public class DocContext : DbContext
     {
+        /// <summary>
+        /// default
+        /// </summary>
+        /// <param name="options"></param>
         public DocContext(DbContextOptions options)
             : base(options)
         {
 
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new ProjectCfg());
         }
 
-        public DbSet<Project> Blogs { get; set; }
+        /// <summary>
+        /// 项目列表
+        /// </summary>
+        public DbSet<Project> Projects { get; set; }
     }
 }
