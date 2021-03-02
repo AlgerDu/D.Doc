e510fee76b9d05b00b0e48c5350886b7f8688fe5
Alger <AlgerDu@outlook.com>
2021-03-02 22:43:45 +0800

    Create ProjectController.cs

---
+++ Src/D.Doc/D.Doc.Server/Controllers/ProjectController.cs
@@ -0,0 +1,30 @@
+﻿using D.Doc.Domain.PO;
+using D.Doc.Domain.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace D.Doc.Server.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class ProjectController : ControllerBase
+    {
+        readonly DocContext _context;
+
+        public ProjectController(
+            DocContext context
+            )
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Project> Get()
+        {
+            return _context.Projects;
+        }
+    }
+}
