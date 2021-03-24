85fef74ad3737baf87d578875c57f8884897f7d4
Alger <AlgerDu@outlook.com>
2021-03-24 22:20:25 +0800

    Create DomainProject.cs

---
+++ Src/D.Doc/D.Doc.Core/Domain/Project/DomainProject.cs
@@ -0,0 +1,52 @@
+﻿using D.Doc.Domain.PO;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace D.Doc.Core
+{
+    public class DomainProject : IProject
+    {
+        readonly ILogger _logger;
+        readonly IEfContextFactory _contextFactory;
+
+        readonly Project _db;
+
+        readonly List<IProjectVersion> _versions;
+
+        #region IProject 属性
+        public long PK => _db.PK;
+
+        public string Name { get => _db.Name; set => _db.Name = value; }
+
+        public string Description { get => _db.Description; set => _db.Description = value; }
+
+        public IProjectVersion CurrentVersion => _versions.FirstOrDefault(vv => vv.PK == _db.CurrVersionId);
+
+        public IEnumerable<IProjectVersion> Versions => _versions;
+        #endregion
+
+        public DomainProject(
+            ILogger<DomainProject> logger
+            , IEfContextFactory contextFactory
+            , Project db
+            )
+        {
+            _logger = logger;
+            _contextFactory = contextFactory;
+            _db = db;
+        }
+
+        public bool IsTransient()
+        {
+            return _db.IsTransient();
+        }
+
+        private void LoadAllVsersion()
+        {
+
+        }
+    }
+}
