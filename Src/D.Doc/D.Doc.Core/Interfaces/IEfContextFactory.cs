7e07a84a0bbacb84457af8156c21013a5dde2e88
Alger <AlgerDu@outlook.com>
2021-03-24 21:51:02 +0800

    Create IEfContextFactory.cs

---
+++ Src/D.Doc/D.Doc.Core/Interfaces/IEfContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using D.Doc.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D.Doc.Core
+{
+    public interface IEfContextFactory
+    {
+        DocContext Create();
+    }
+}
